1d6242b5e386e138236625450cd428195d336f8b
Pia <piatorain@yahoo.com>
2022-09-22 16:59:01 -0700

    adding changes

---
+++ unit1/part4-OIP/javaPolymorphism/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+// Polymorphism
+    // many forms
+    // obj can take many forms
+    // inheritance allows an obj to be polymorphic
+        // its own form or the form of the class it inherits
