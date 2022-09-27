05532efb51f9202a4ce825d2224d60fbbdb49772
Pia <piatorain@yahoo.com>
2022-09-27 11:08:45 -0700

    unbound wildcards and bounded type params

---
+++ unit3/3-Wildcards/1-UnboundWildcards/src/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle implements Shape {
+
+    // methods
+    // must override the draw method
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
