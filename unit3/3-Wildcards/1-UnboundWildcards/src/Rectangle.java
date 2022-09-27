5a0fd8be46b7863c9a76b4c6c009fc5be61afea5
Pia <piatorain@yahoo.com>
2022-09-27 15:11:05 -0700

    update files

--- unit3/3-Wildcards/1-UnboundWildcards/src/Rectangle.java
+++
@@ -1,9 +0,0 @@
-public class Rectangle implements Shape {
-
-    // methods
-    // must override the draw method
-    @Override
-    public void draw() {
-        System.out.println("Drawing a rectangle");
-    }
-}
--- unit3/3-Wildcards/1-UnboundWildcards/src/Shape.java
+++
@@ -1,4 +0,0 @@
-public interface Shape {
-    // methods
-    public void draw();
-}
