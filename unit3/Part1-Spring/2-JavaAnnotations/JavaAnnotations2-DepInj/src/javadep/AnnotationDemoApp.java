85114c32f08a337b4f5ca74cc2e302163de4e682
Pia <piatorain@yahoo.com>
2022-09-30 11:25:10 -0700

    qualifies with bean names, constructors and property files

--- unit3/Part1-Spring/2-JavaAnnotations/JavaAnnotations2-DepInj/src/javadep/AnnotationDemoApp.java
+++ unit3/Part1-Spring/2-JavaAnnotations/JavaAnnotations2-DepInj/src/javadep/AnnotationDemoApp.java
@@ -30,5 +30,23 @@ public class AnnotationDemoApp {
 		// inject dependencies by setting field values directly into the class
 		// uses Java Reflection to inject dependencies
 		// NO NEED FOR SETTER METHODS
+	// Default Bean names
+		// if the annotation's value doesn't indicate a bean name, an appropriate name
+		// will be built based on the short name of the class (with the first letter lower-cased).
+		// Java Beans Introspector - https://docs.oracle.com/javase/7/docs/api/java/beans/Introspector.html
+		// EX HappyFortuneService --> happyFortuneService
+	// special case of when BOTH the first and second characters of the class name
+		// are upper case, then the name is NOT converted.
+		// EX: RESTFortuneService --> RESTFortuneService
+	// Qualifies with constructors
+		// @Autowired
+		// public TennisCoach(@Qualifier("randomFortuneService") FortuneService theFortuneService) {
+		// 	fortuneService = theFortuneService;
+		// }
+	// Injecting values from properties file
+		// @Value("${foo.email}")
+		// private String email;
+		// @Value("${foo.team}")
+		// private String team;
 
 
