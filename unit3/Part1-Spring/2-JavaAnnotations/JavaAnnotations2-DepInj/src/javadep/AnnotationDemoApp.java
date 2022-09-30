2c4d55366dd9c704f65e5f2c8da985b927b6c902
Pia <piatorain@yahoo.com>
2022-09-30 08:40:49 -0700

    java anno constructor method edit

--- unit3/Part1-Spring/2-JavaAnnotations/JavaAnnotations2-DepInj/src/javadep/AnnotationDemoApp.java
+++ unit3/Part1-Spring/2-JavaAnnotations/JavaAnnotations2-DepInj/src/javadep/AnnotationDemoApp.java
@@ -23,7 +23,27 @@ public class AnnotationDemoApp {
 		context.close();
 		
 	}
-
 }
+
+// Java Annotations - Dependency Injection and Autowiring
+	// Autowiring - Note in spring 4.3 @Autowired is no longer required
+		// Spring will automatically wire objects together
+		// How?
+			// Spring will look for a class that mtches the propperty
+				// matches by type: class or interface
+			// Spring find the match and will automatically inject it
+	// Spring will scan for a component that implements FortuneService
+	// Spring will inject that dependency automatically
+	// If multiple implementations of an Interface are found
+		// use @Qualifier to specify which implementation to use
+	// Autowiring Injection Types
+		// Constructor Injection
+		// Setter Injection
+		// Field Injection
+	// Constructor Injection - TennisCoach.java
+		// inject dependencies by calling setter methods on the
+		//1. Define the dependency interface and class
+		//2. Create a constructor in your class for injection
+		//3. Configure the dependency injection with @Autowired
 
 
--- unit3/Part1-Spring/2-JavaAnnotations/JavaAnnotations2-DepInj/src/javadep/Main.java
+++
@@ -1,7 +0,0 @@
-package javadep;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
