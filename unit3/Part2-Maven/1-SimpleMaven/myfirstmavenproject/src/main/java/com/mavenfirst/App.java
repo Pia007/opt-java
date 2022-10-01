06f936148b43b6520b5affb786391576016db9b9
Pia <piatorain@yahoo.com>
2022-10-01 13:20:02 -0700

    simple maven project using quickstart archetype

---
+++ unit3/Part2-Maven/1-SimpleMaven/myfirstmavenproject/src/main/java/com/mavenfirst/App.java
@@ -0,0 +1,13 @@
+package com.mavenfirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from inside of my first Maven Project!" );
+    }
+}
