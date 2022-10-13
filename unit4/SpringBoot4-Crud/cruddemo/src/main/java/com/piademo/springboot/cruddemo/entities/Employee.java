822df6e9ceb16fae92384cf96bc775911be214c9
Pia <piatorain@yahoo.com>
2022-10-12 17:13:01 -0700

    project setup

---
+++ unit4/SpringBoot4-Crud/cruddemo/src/main/java/com/piademo/springboot/cruddemo/entities/Employee.java
@@ -0,0 +1,71 @@
+package com.piademo.springboot.cruddemo.entities;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+
+@Entity
+@Table(name="employees")
+
+public class Employee {
+
+    // fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    // constructors
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // getter and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
