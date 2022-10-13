3befd8dbf7bdf457783491fd11999920ee5b9f24
Pia <piatorain@yahoo.com>
2022-10-13 16:48:49 -0700

    Spring Date REST API and HATEOAS

---
+++ unit4/SpringBoot7-Crud-SpringDataREST/cruddemospringdatarest/src/main/java/com/piademo/springboot/cruddemo/CruddemoSpringDataRESTApplication.java
@@ -0,0 +1,13 @@
+package com.piademo.springboot.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoSpringDataRESTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoSpringDataRESTApplication.class, args);
+	}
+
+}
