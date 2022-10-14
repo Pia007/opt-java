af42012d0aceeaa4181cad154ab9b1670dc978b8
Pia <piatorain@yahoo.com>
2022-10-13 17:46:31 -0700

    Spring Data REST Configuration, Pagination and Sorting

--- unit4/SpringBoot7-Crud-SpringDataREST/cruddemospringdatarest/src/main/java/com/piademo/springboot/cruddemo/dao/EmployeeRepository.java
+++ unit4/SpringBoot7-Crud-SpringDataREST/cruddemospringdatarest/src/main/java/com/piademo/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,10 +2,11 @@ package com.piademo.springboot.cruddemo.dao;
 
 import com.piademo.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-/*13 Removed all DAO files - create employee repository */
+/*Changing Rest resource path name*/
 
-
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // no need to write any code
 }
