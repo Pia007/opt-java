2e74964b028a398712f6356af071458d78d7134e
Pia <piatorain@yahoo.com>
2022-10-17 12:20:30 -0700

    challenge desc merge sort

---
+++ unit5/2-SortAlgorithms/G-Sort-Algo-Challenge/src/academy/learnprogramming/challenge1/Main.java
@@ -0,0 +1,53 @@
+package academy.learnprogramming.challenge1;
+/*
+ Modify the merge sort algorithm so that ir sorts integers in descending order
+* */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        mergeSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void mergeSort(int[] input, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    public static void merge(int[] input, int start, int mid, int end) {
+
+        // if last element in the left partition is >= first element in the right partition just return
+
+        if (input[mid - 1] >= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            // if input at index 1 is >= input at index j, incr i++, otherwise ince j ++
+            temp[tempIndex++] = input[i] >= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+
+    }
+
+}
