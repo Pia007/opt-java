5ba048943fcb95697e3f2c1a9809bc40e494895c
Pia <piatorain@yahoo.com>
2022-10-19 19:23:54 -0700

    vectors

---
+++ unit5/3-Lists/B-Vectors/src/academy/learnprogramming/vectors/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming.vectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // implement a Vector class
+        List<Employee> employeeList = new Vector<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
+
+
+
+    }
+}
