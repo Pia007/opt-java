026ba90ab6366945a69ad3f26c9a801b20368be0
Pia <piatorain@yahoo.com>
2022-10-20 11:55:39 -0700

    exceptions

--- unit5/6-JUnitTesting/JUnit-Opt/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ unit5/6-JUnitTesting/JUnit-Opt/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -17,10 +17,31 @@ class ArraysCompareTest {
         Arrays.sort(numbers);
 
         //fails bc it checks if they are the same object
-//        assertEquals(expected, numbers);
+        //assertEquals(expected, numbers);
 
         // to compare the values in arrays use assertArrayEquals
         assertArrayEquals(expected, numbers);
+
+
+    }
+
+    @Test
+     void testArraySort_NullArray() {
+        int[] numbers = {};
+//        int[] numbers = null;
+//        Arrays.sort(numbers);
+        int[] expected = {3, 4, 12, 18};
+        try {
+            Arrays.sort(numbers);
+        } catch (NullPointerException e) {
+
+        }
+
+
+        // test exceptions by changing the numbers array to null
+        // use try-catch block and test will succeed bc NullPointer Exception is expected
+        // better way - is to user @Test(expected = NullPointerException.class)  does not work in java 18
+        // empty array {} will not throw an expection, thus failed
     }
 
 }
